69cfb75cc95a5315e23410fff7253de725debc55
anupkumarjana <janaanup7872@gmail.com>
2024-03-16 13:51:31 +0530

    created a hook to fetch the estaurant head data

---
+++ src/utils/useRestaurantMenuHead.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { menuApi } from "../constants";
+
+const useReataurantMenuHead = (restaurantId) => {
+  const [resData, setResData] = useState({});
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(`${menuApi}${restaurantId}`);
+    const jsonData = await data.json();
+
+    const ResturantData = await jsonData.data?.cards[0]?.card?.card?.info;
+    setResData(ResturantData);
+  };
+  return resData;
+};
+
+export default useReataurantMenuHead;
